3ce2641d75e6314124a7e95977dcfda452c472c1
Kristen Skruber <skruberk@gmail.com>
2024-05-14 15:33:54 -0700

    Add files via upload
    turns fasta to csv

---
+++ fasta2csv.R
@@ -0,0 +1,21 @@
+
+input <- readLines("profilins-all.fas")
+output <- file("output.csv","w")
+
+currentSeq <- 0
+newLine <- 0
+
+for(i in 1:length(input)) {
+  if(strtrim(input[i], 1) == ">") {
+    if(currentSeq == 0) {
+      writeLines(paste(input[i],"\t"), output, sep="")
+      currentSeq <- currentSeq + 1
+    } else {
+      writeLines(paste("\n",input[i],"\t", sep =""), output, sep="")
+    }
+  } else {
+    writeLines(paste(input[i]), output, sep="")
+  }
+}
+
+close(output)
