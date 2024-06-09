edfe9e4ca7fdc844734241089032ffd61adea908
Kristen Skruber <skruberk@gmail.com>
2024-06-09 15:13:17 -0700

    Add files via upload
    common and general data cleaning for working with dataframes

---
+++ DataClean/dataclean_general.R
@@ -0,0 +1,36 @@
+#general data cleaning 
+#cmd shift R makes new section, cmd shift m for pipe
+
+# #subset by row  ---------------------------------------------------------
+filtered<-filter(dataset,column=="category of one variable")
+filtered<-filter(dataset,column > 10000)
+filtered<-filter(dataset,column > 10000 | column=="category of one variable")
+#can filter using is.na or !is.na
+
+# #subset by column  ------------------------------------------------------
+subsetted<-select(dataset, column1,column2)
+subsetted<-select(dataset, 1:2) #same thing columns 1 and 2
+subsetted<-select(dataset,starts_with("ex")) #can do contains() or ends_with() 
+subsetted<-select(dataset,-column1) #column1 here is the variable name
+subsetted<-dataset %>%
+  select(-column1) %>% #can just pipe instead cmd shift m
+
+
+# #reorder row ------------------------------------------------------------
+arranged<- dataset %>% arrange(variable1,variable2) #defaults to alphaebetical
+arranged<- dataset %>% arrange(desc(variable1,variable2)) #change to descending
+
+
+# #add/modify columns -----------------------------------------------------
+modified<-dataset %>% mutate(newdatacolumn = 0.5 * existingcolumn) #multiply by whatever
+modified<-dataset %>% mutate(newdatacolumn = existingcolumn1 / existingcolumn2)
+
+
+# other stuff -------------------------------------------------------------
+#slice() index rows by integer locations you can slice_min() and slice_max()
+#bind() to join datasets together
+#rename() rename columns
+
+#summaries
+#group_by()
+#summarise()
