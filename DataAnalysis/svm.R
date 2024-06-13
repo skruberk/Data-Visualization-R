19537f74eaef955605eaec94d1bea23df63646ed
Kristen Skruber <skruberk@gmail.com>
2024-06-12 18:34:00 -0700

    Add files via upload

--- DataAnalysis/svm.R
+++ DataAnalysis/svm.R
@@ -30,7 +30,7 @@ View(training_set)
 View(test_set)
 # Fitting SVM to the Training set
 
-classifier = svm(formula = Purchased ~ .,
+classifier <- svm(formula = Purchased ~ .,
                  data = training_set,
                  type = 'C-classification',
                  kernel = 'linear')
@@ -40,33 +40,36 @@ y_pred = predict(classifier, newdata = test_set[-3])
 
 # Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred)
+cm
 
-# Visualising the Training set results
+# Define grid of points for prediction
+set <- training_set
+X1 <- seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 <- seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set <- expand.grid(X1, X2)
+colnames(grid_set) <- c('Age', 'EstimatedSalary')
 
-set = training_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3],
-     main = 'SVM (Training set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
-     xlim = range(X1), ylim = range(X2))
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'dodgerblue', 'salmon'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
+# Predict on the grid
+y_grid <- predict(classifier, newdata = grid_set)
+grid_set$Purchased <- y_grid  # Add predictions to grid data
 
-# Visualising the Test set results
-set = test_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3], main = 'SVM (Test set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
-     xlim = range(X1), ylim = range(X2))
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'dodgerblue', 'salmon'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
+
+# training set results
+p<-ggplot() +
+  geom_raster(data = grid_set, aes(x = Age, y = EstimatedSalary, fill = Purchased), alpha = 0.5) +
+  geom_point(data = training_set, aes(x = Age, y = EstimatedSalary, color = as.factor(Purchased)), size = 3, show.legend=FALSE) +
+  scale_fill_manual(values = c("0" = "salmon", "1" = "dodgerblue")) +
+  scale_color_manual(values = c("0" = "salmon3", "1" = "dodgerblue3")) +
+  labs(title = 'SVM (Training set)', x = 'Age', y = 'Estimated Salary') +
+  theme_bw()+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+p
+
+# test set results
+p<-ggplot() +
+  geom_raster(data = grid_set, aes(x = Age, y = EstimatedSalary, fill = Purchased), alpha = 0.5) +
+  geom_point(data = test_set, aes(x = Age, y = EstimatedSalary, color = as.factor(Purchased)), size = 3, show.legend=FALSE) +
+  scale_fill_manual(values = c("0" = "salmon", "1" = "dodgerblue")) +
+  scale_color_manual(values = c("0" = "salmon3", "1" = "dodgerblue3")) +
+  labs(title = 'SVM (Test set)', x = 'Age', y = 'Estimated Salary') +
+  theme_bw()+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+p
