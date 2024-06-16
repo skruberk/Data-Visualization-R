5a0b72ce3591970577a644c367f9eab8e3e731b9
Kristen Skruber <skruberk@gmail.com>
2024-06-15 19:12:44 -0700

    Add files via upload
    general script for database queries

---
+++ DataClean/SQL.R
@@ -0,0 +1,8 @@
+##SQL database general, tailor to specific use cases
+library(RODBC)
+myconn<-odbcConnect("Database_Name", uid="User_ID",pwd="password")
+dat<-sqlFetch(myconn,"Table_Name")
+querydat<-sqlQuery(myconn,"SELECT * FROM table")
+close(myconn)
+
+library(RMySQL)
